d1d55ab66b700a5ebc81902bbf763b07e27ce565
Justine120918 <camarillocombisjustine@gmail.com>
2024-09-09 03:26:56 +0000

    Initial commit with app.js

---
+++ variable.js
@@ -0,0 +1,10 @@
+
+let name = "Justin combis";
+let whatDoYouWannaBecomeInYourLife = "programmer";
+let gender = "19";
+let twitterHandle = "JUSTINE7559074";
+
+console.log(name);
+console.log(whatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(twitterHandle);
