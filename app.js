65f7996ed0e54ad70bc2bfc65a570a81d0e69aaf
Justine120918 <camarillocombisjustine@gmail.com>
2024-09-09 00:53:22 +0000

    Initial commit with app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+console.log('Hello, World!');
+name = "Justine Combis ";
+console.log('My name is ' + name);
+
+// Variables
+let num1 = 10;
+let num2 = 20;
+
+// Boolean value
+let isTrue = true;
+let isFalse = false;
+
+// Comparison operators
+let isEqual = (num1 === num2);  
+let isNotEqual = (num1 !== num2); 
+let isGreater = (num1 > num2);  
+let isLess = (num1 < num2); 
+
+// Logical operation using Booleans
+let result = isTrue && isNotEqual; 
+let result2 = isTrue || isFalse; 
+
+// Output the results
+console.log(isEqual);    
+console.log(isNotEqual);   
+console.log(isGreater);      
+console.log(isLess);       
+console.log(result);
+console.log(result2);
