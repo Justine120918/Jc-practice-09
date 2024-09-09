70a64778eb7ea08d864732e5ca44db363ef4c89a
Justine120918 <camarillocombisjustine@gmail.com>
2024-09-09 03:55:47 +0000

    Initial commit with app.js

---
+++ numbers.js
@@ -0,0 +1,26 @@
+let firstNum = 9; 
+let secondNum = 22;
+
+// Addition
+let addition = firstNum + secondNum;
+console.log(`Addition: ${firstNum} + ${secondNum} = ${addition}`);
+
+// Subtraction
+let subtraction = firstNum - secondNum;
+console.log(`Subtraction: ${firstNum} - ${secondNum} = ${subtraction}`);
+
+// Multiplication
+let multiplication = firstNum * secondNum;
+console.log(`Multiplication: ${firstNum} * ${secondNum} = ${multiplication}`);
+
+// Division
+let division = firstNum / secondNum;
+console.log(`Division: ${firstNum} / ${secondNum} = ${division}`);
+
+// Modulus
+let modulus = firstNum % secondNum;
+console.log(`Modulus: ${firstNum} % ${secondNum} = ${modulus}`);
+
+// Exponentiation (Power)
+let exponentiation = firstNum ** secondNum;
+console.log(`Exponentiation: ${firstNum} ** ${secondNum} = ${exponentiation}`);
