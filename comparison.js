161fc034b6531e48608f5b6825118154e291e579
Justine120918 <camarillocombisjustine@gmail.com>
2024-09-10 03:30:09 +0000

    Initial commit with app.js

---
+++ comparison.js
@@ -0,0 +1,37 @@
+let firstFavNumb = 7;
+
+let secondFavNumb = 3;
+
+
+// Greater than
+console.log("Greater than:", firstFavNumb > secondFavNumb);
+
+// Less than
+console.log("Less than:", firstFavNumb < secondFavNumb);
+
+// Greater than or equal to
+console.log("Greater than or equal to:", firstFavNumb >= secondFavNumb);
+
+// Less than or equal to
+console.log("Less than or equal to:", firstFavNumb <= secondFavNumb);
+
+// Strict equality (===)
+console.log("Strict equality (===):", firstFavNumb === secondFavNumb);
+
+// Loose equality (==)
+console.log("Loose equality (==):", firstFavNumb == secondFavNumb);
+
+// Strict inequality (!==)
+console.log("Strict inequality (!==):", firstFavNumb !== secondFavNumb);
+
+// Loose inequality (!=)
+console.log("Loose inequality (!=):", firstFavNumb != secondFavNumb);
+
+// AND (both numbers are greater than 0)
+console.log("AND:", firstFavNumb > 0 && secondFavNumb > 0);
+
+// OR (either number is greater than 5)
+console.log("OR:", firstFavNumb > 5 || secondFavNumb > 5);
+
+// NOT (firstFavNumb is not equal to 5)
+console.log("NOT:", !(firstFavNumb === 5));
